ad4b19ea4a13d1915ba01683b23016062c8156c7
GraysonNZ <aasish.khanal@yahoo.com>
2016-09-23 10:57:20 +1200

    correct spell

--- home.php
+++ home.php
@@ -57,7 +57,7 @@ include ('header.php'); ?>
          <div id= "message_area"> </div>
          <div id= "note">
            <hr>
-           <p> Note: This database will not save to the database. The database server is currently inactive. (This page is using google maps places api, typescript and bootstrap) </p>
+           <p> Note: This data will not save to the database. The database server is currently inactive. (This page is using google maps places api, typescript and bootstrap) </p>
          </div>
 
        </div>
